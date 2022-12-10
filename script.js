dd94b2a50a81b66b2d0730e302a447817827ed9b
Asartea <76259120+Asartea@users.noreply.github.com>
2022-12-10 14:26:45 +0100

    more refactoring

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function Book(title, author, pages, read) {
 }
 function Library() {
     this.books = []
-    this.addBook = function(newBook) {
-        if(!this.alreadyInLibrary(newBook)) {
-            this.books.push(newBook)
+    this.addBook = function(book) {
+        if(!this.alreadyInLibrary(book)) {
+            this.books.push(book)
         }
     }
     this.removeBook = function(title) {
@@ -27,8 +27,13 @@ function Controller() {
     this.Display = new Display()
     this.addBook = function(formData) {
         let data = this.handleFormData(formData)
+        if(this.Library.alreadyInLibrary(data)) {
+            console.error("cant add books with the same title")
+            return
+        }
+        data.read = data.read ? true : false
         let newBook = new Book(data.title, data.author, data.pages, data.read)
-        this.Display.addBook(newBook)
+        this.Display.renderBook(newBook)
         this.Library.addBook(newBook)
     }
     this.removeBook = function(title) {
@@ -46,32 +51,35 @@ let mainController = new Controller()
 function Display() {
     this.template = document.querySelector('template')
     this.container = document.getElementById('container')
-    this.addBook = function(newBook) {
+    this.submitButton = document.getElementById('form-submit')
+    this.renderBook = function(book) {
         let clone = this.template.content.cloneNode(true)
         let bookCard = clone.querySelectorAll('.book-card')[0]
-        bookCard.dataset.title = newBook.title
+        bookCard.dataset.title = book.title
         let rows = bookCard.querySelectorAll('.bookcard-row')
-        rows[0].textContent = newBook.title
-        rows[1].textContent = newBook.author
-        rows[2].textContent = newBook.pages
+        rows[0].textContent = book.title
+        rows[1].textContent = book.author
+        rows[2].textContent = book.pages
         let removeButton = bookCard.querySelector('#remove-button')
-        removeButton.dataset.title = newBook.title
+        removeButton.dataset.title = book.title
         console.log(removeButton)
         removeButton.addEventListener('click', e => {
             let title = e.target.parentElement.dataset.title
-            console.log(e.target.parentElement.dataset.title)
             mainController.removeBook(title)
        })
        let readButton = bookCard.querySelector("#read-indicator")
-       let bookIsRead = newBook.read ? true : false
-       let bookIsReadString = bookIsRead ? "Read" : "Unread"
+       let bookIsReadString = book.read ? "Read" : "Unread"
+       console.log(readButton)
        readButton.textContent = bookIsReadString
-       console.log(bookIsRead)
-       this.container.appendChild(clone)
+       this.container.appendChild(bookCard)  
     }
     this.removeBook = function(title) {
         let bookCard = document.querySelector(`[data-title=${title}]`)
         bookCard.remove()
+    }
+    this.updateBook= function(book) {
+        this.removeBook(book.title)
+        this.renderBook(book)
     }
 }
 submitButton.addEventListener('click', (e) => {
